f42134e6266f716d779f8bc24ea9febf8791d07c
sushant-batra <119495514+sushant-batra@users.noreply.github.com>
2022-11-30 22:16:37 +0530

    Add files via upload

---
+++ odd_even.py
@@ -0,0 +1,14 @@
+import streamlit as st
+st.title("Odd or Even")
+st.header("You can check whether a  number is odd or even")
+number = st.number_input("Enter a positive integer that you wish to check")
+if(st.button('Submit')):
+    if (number.is_integer() and number>0):
+        result = number%2
+        if (result==1):
+            st.title("Number is odd")
+        else:
+            st.title("Number is even")
+            st.success(result)
+    else:
+        st.title("Enter a valid number")
